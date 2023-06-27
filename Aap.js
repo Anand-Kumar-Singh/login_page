bbef6cedd4807ec33dd18b07f7ef13613f8337ad
anand_kumar_singh13 <anandkumarsingh1342@gmail.com>
2023-06-27 22:00:15 +0530

    'aap'

---
+++ Aap.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM, { createRoot } from "react-dom/client";
+const Aap = () => {
+  return <div>A</div>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Aap />);
