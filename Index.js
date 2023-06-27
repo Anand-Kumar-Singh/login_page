64647f7fc556d4454dbffd7e4deec9fd4e52983d
anand_kumar_singh13 <anandkumarsingh1342@gmail.com>
2023-06-27 21:06:52 +0530

    'index.js'

--- Index.js
+++ Index.js
@@ -1,6 +1,6 @@
 import React, { Suspense } from "react";
 import ReactDOM, { createRoot } from "react-dom/client";
-import Login from "./components/Login";
+import Login from "./src/components/Login";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const Index = () => {
